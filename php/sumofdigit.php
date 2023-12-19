3f6de59ba47a68ac5483532c097a65c48807db90
Nishanabanu <147145402+Nishanabanu@users.noreply.github.com>
2023-12-19 15:54:52 +0530

    Add files via upload

---
+++ php/sumofdigit.php
@@ -0,0 +1,16 @@
+
+  <html>
+  <body>
+  <?php  
+  $num = 12345;  
+  $sum=0; $rem=0;  
+  for ($i =0; $i<=strlen($num);$i++)  
+  {  
+  $rem=$num%10;  
+  $sum = $sum + $rem;  
+  $num=$num/10;  
+  }  
+  echo "Sum of digit is $sum";  
+  ?>  
+  </body>
+  </html>
